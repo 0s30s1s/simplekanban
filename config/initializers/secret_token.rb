eb587410d3deb44b35dc7bfd84fb8172a192b406
sathish <sahthish316@gmail.com>
2010-12-15 20:33:41 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplekanban::Application.config.secret_token = 'cc126ef7826b07cdda680e0d9c22cfef9ff94cb7592c5bdcb1e61d78538d0beeea5f2bdb2d92873da4d100843d19faf806d1ff50fde1547785c38aa80dc72a8f'
